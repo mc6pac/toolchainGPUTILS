f6a995671b19df8b741c3f993d29f3580a1175b2
mc6pac <mc6pac@gmail.com>
2019-08-30 22:00:48 +0200

    Fix for MPLABX 5.xx
    Bugfix to support MPLAB X v5.xx

--- src/com/github/mc6pac/toolchainGPUTILS/GPUTILSSystemIncludeProvider.java
+++ src/com/github/mc6pac/toolchainGPUTILS/GPUTILSSystemIncludeProvider.java
@@ -1,7 +1,7 @@
 package com.github.mc6pac.toolchainGPUTILS;
 
 import com.microchip.mplab.nbide.embedded.spi.IncludeProvider;
-import com.microchip.mplab.nbide.toolchainCommon.LTUtils;
+import com.microchip.mplab.nbide.toolchainCommon.properties.CommonLanguageToolchainPropertiesUtils;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +9,9 @@ import org.netbeans.api.project.Project;
 import org.netbeans.spi.project.ProjectConfiguration;
 
 public class GPUTILSSystemIncludeProvider implements IncludeProvider {
-    
+
+    private final CommonLanguageToolchainPropertiesUtils utils = new CommonLanguageToolchainPropertiesUtils();
+
     public GPUTILSSystemIncludeProvider() {
     }
     
@@ -17,7 +19,7 @@ public class GPUTILSSystemIncludeProvider implements IncludeProvider {
     public List<String> getIncludes(Project project, ProjectConfiguration projectConf, String itemPath) {
       
         final List<String> ret = new ArrayList<>();
-        final String pathToBin = LTUtils.pathToBin(projectConf);
+        final String pathToBin = utils.pathToBin(projectConf);
         ret.add(pathToBin + File.separator + ".." + File.separator + "header");
         return ret;
     }
